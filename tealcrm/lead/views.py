88172468f9da9ed8c838ff4f61d7aa71d2f2028c
Phone Aung <phonexaung@gmail.com>
2023-05-25 21:30:31 +0630

    add Views/Forms for leads

--- tealcrm/lead/views.py
+++ tealcrm/lead/views.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import AddLeadForm
 
 @login_required
 def add_lead(request):
-    form = AddLeadForm()
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard')
+    else:
+        form = AddLeadForm()
 
     return render(request, 'lead/add_lead.html', {
         'form': form
