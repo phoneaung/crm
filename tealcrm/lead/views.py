e8ea19202a19ef6a0e0f99f5aa1ff7140b9a473a
phoneaung <phonexaung@gmail.com>
2023-05-28 00:23:18 +0630

    update leads_list page

--- tealcrm/lead/views.py
+++ tealcrm/lead/views.py
@@ -9,7 +9,7 @@ from client.models import Client
 # shows the list of all the leads created by user
 @login_required
 def leads_list(request):
-    leads = Lead.objects.filter(created_by=request.user)
+    leads = Lead.objects.filter(created_by=request.user, converted_to_client=False)
 
     return render(request, 'lead/leads_list.html', {
         'leads': leads
