8b55733d775f460c77fd6bcc7da93579cd8fb967
phoneaung <phonexaung@gmail.com>
2023-05-27 23:50:37 +0630

    update client model

--- tealcrm/client/models.py
+++ tealcrm/client/models.py
@@ -1,35 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Lead(models.Model):
-    LOW = 'low'
-    MEDIUM = 'medium'
-    HIGH = 'high'
-
-    CHOICES_PRIORITY = (
-        (LOW, 'Low'),
-        (MEDIUM, 'Medium'),
-        (HIGH, 'High'),
-    )
-
-    NEW = 'new'
-    CONTACTED = 'contacted'
-    WON = 'won'
-    LOST = 'lost'
-
-    CHOICES_STATUS = (
-        (NEW, 'New'),
-        (CONTACTED, 'Contacted'),
-        (WON, 'Won'),
-        (LOST, 'Lost'),
-    )
-
+class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     description = models.TextField(blank=True, null=True)
-    priority = models.CharField(max_length=10, choices=CHOICES_PRIORITY, default=MEDIUM)
-    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=NEW)
-    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='clients', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
