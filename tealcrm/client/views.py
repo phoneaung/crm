6556fc25a1745d092d7483173a80e34b1ef69c08
phoneaung <phonexaung@gmail.com>
2023-05-28 10:17:15 +0630

    add views for clients

--- tealcrm/client/views.py
+++ tealcrm/client/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import AddClientForm
+from client.models import Client
 
 
+# shows the list of all the clients created by user
+@login_required
+def clients_list(request):
+    clients = Client.objects.filter(created_by=request.user)
+
+    return render(request, 'client/clients_list.html', {
+        'clients': clients
+    })
+
+
+# show the details of a client
+@login_required
+def clients_detail(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    return render(request, 'client/clients_detail.html', {
+        'client': client
+    })
+
+
+# allow the user to add clients
+@login_required
+def add_client(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.created_by = request.user
+            client.save()
+
+            messages.success(request, "The client was created.")
+
+            return redirect('clients_list')
+    else:
+        form = AddClientForm()
+
+    return render(request, 'client/clients_list.html', {
+        'form': form
+    })
+
+
+# allow the user to delete clients
+@login_required
+def clients_delete(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    client.delete()
+    messages.success(request, "The client was deleted.")
+
+    return redirect('clients_list')
+
+
+# allow the user to edit leads
+@login_required
+def clients_edit(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    
+    if request.method == 'POST':
+        form = AddclientForm(request.POST, instance=client)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "The changes were saved.")
+            return redirect('clients_list')
+    else:
+        form = AddclientForm(instance=client)
+ 
+    return render(request, 'client/clients_edit.html', {
+        'form': form
+    })
