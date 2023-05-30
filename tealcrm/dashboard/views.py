b5998645de752d39ab65d725f1615f7a1cc29325
phoneaung <phonexaung@gmail.com>
2023-05-30 10:39:15 +0630

    show leads and clients on dashboard

--- tealcrm/dashboard/views.py
+++ tealcrm/dashboard/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from client.models import Client
+from lead.models import Lead
+from team.models import Team
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    team = pass
+
+    # make these lists order by (-Created at) and show five of them 
+    leads = pass
+    clients = pass
+
+    return render(request, 'dashboard/dashboard.html', {
+        'leads': lead,
+        'clients': client
+    })
